8378e1d0d0032e6b93cf24ead2867d4486d9a59b
Duong Vi Phat <viphat@gmail.com>
2018-05-28 18:18:19 +0700

    Set DEBUG=False for Heroku

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -139,7 +139,9 @@ if os.getcwd() == '/app':
   SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
   # Allow all host headers.
-  ALLOWED_HOSTS = ['*']
+  ALLOWED_HOSTS = ['viphat-learning-log.herokuapp.com']
+
+  DEBUG = False
 
   # Static asset configuration
   BASE_DIR = os.path.dirname(os.path.abspath(__file__))
